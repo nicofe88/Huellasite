626dd2594a83316091de31bb62eb9ee5e51fa4d5
FerEquis <fcastilloaguirre@gmail.com>
2022-10-29 21:09:55 -0300

    Va con html red Huella
    api fetch

--- script.js
+++ script.js
@@ -5,3 +5,19 @@ function validarForm() {
       return false;
     }
   }
+
+
+  function traer() {
+    fetch('https://randomuser.me/api')
+      .then(res => res.json())
+      .then(res => {
+         console.log(res)
+         console.log(res.results[0].email)
+         contenido.innerHTML= `
+           <img src="${res.results[0].picture.large}
+             "width="300px" 
+             class="img-fluid rounded-circle">
+           <p>Nombre: ${res.results[0].name.first}</p>
+           <p>Mail: ${res.results[0].email}</p>`
+      })
+   }
